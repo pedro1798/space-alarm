6745ac518a773f66531003c1e28e44c028fda287
pedro1798 <peter584aa@knu.ac.kr>
2025-04-30 16:41:25 +0900

    add permission hander

--- lib/handers/location_permission_hander.dart
+++ lib/handers/location_permission_hander.dart
@@ -1,14 +1,18 @@
 import 'package:permission_handler/permission_handler.dart';
 
 Future<void> requestLocationPermissions() async {
-  final status = await Permission.locationAlways.status;
+  // 위치 권한이 이미 허용되었는지 확인
+  PermissionStatus status = await Permission.location.request();
 
-  if (!status.isGranted) {
-    final result = await Permission.locationAlways.request();
-    if (result.isGranted) {
-      print('위치 권한 허용됨');
-    } else {
-      print('위치 권한 거부됨');
-    }
+  if (status.isDenied) {
+    // 권한이 거부되었을 경우
+    print("위치 권한 거부됨");
+  } else if (status.isGranted) {
+    // 권한이 허용되었을 경우
+    print("위치 권한 허용됨");
+  } else if (status.isPermanentlyDenied) {
+    // 권한이 영구적으로 거부되었을 경우
+    print("위치 권한 영구적으로 거부됨");
+    // openAppSettings(); // 앱 설정 화면으로 이동하여 수동으로 권한을 변경하도록 유도
   }
 }
