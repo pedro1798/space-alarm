656bce39603818aa3111a75a0542d3d73d7d11a1
pedro1798 <peter584aa@knu.ac.kr>
2025-05-02 19:08:53 +0900

    change name field

--- lib/widgets/location_list_widget.dart
+++ lib/widgets/location_list_widget.dart
@@ -1,42 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:plata/services/location_db.dart';
-import 'package:plata/models/stored_location.dart';
 import 'package:get/get.dart';
 import 'package:plata/controllers/location_controller.dart';
 
-class LocationListWidget extends StatefulWidget {
+class LocationListWidget extends StatelessWidget {
   const LocationListWidget({super.key});
 
   @override
-  State<LocationListWidget> createState() => _LocationListWidgetState();
-}
-
-class _LocationListWidgetState extends State<LocationListWidget> {
-  List<StoredLocation> locations = [];
-  final LocationController controller = Get.find<LocationController>();
-
-  @override
-  void initState() {
-    super.initState();
-    _loadLocations();
-  }
-
-  Future<void> _loadLocations() async {
-    final data = await LocationDatabase.getLocations();
-    setState(() {
-      locations = data;
-    });
-  }
-
-  Future<void> _deleteLocation(String id) async {
-    await LocationDatabase.deleteLocation(id);
-    _loadLocations(); // 또는 setState(() => locations.removeWhere((l) => l.id == id));
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final LocationController controller = Get.find<LocationController>();
+
     return Obx(() {
+      if (controller.isLoading.value) {
+        return const Center(child: CircularProgressIndicator());
+      }
+
       final locations = controller.locations;
+
       if (locations.isEmpty) {
         return const Center(child: Text('There are no locations registered.'));
       }
@@ -47,9 +26,9 @@ class _LocationListWidgetState extends State<LocationListWidget> {
           final loc = locations[index];
           return ListTile(
             leading: const Icon(Icons.location_on),
-            title: Text('ID: ${loc.id}'),
+            title: Text('Name: ${loc.name}'),
             subtitle: Text(
-              '위도: ${loc.latitude}, 경도: ${loc.longitude}, 반경: ${loc.radius}m',
+              'Latitude: ${loc.latitude}, Longitude: ${loc.longitude}, Radius: ${loc.radius}m',
             ),
             trailing: IconButton(
               icon: const Icon(Icons.delete, color: Colors.red),
--- lib/widgets/location_register_widget.dart
+++ lib/widgets/location_register_widget.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:plata/handers/geofence_register.dart' as geofenceRegister;
 
 class LocationRegisterWidget extends StatelessWidget {
+  final TextEditingController nameController;
   final TextEditingController latController;
   final TextEditingController longController;
   final TextEditingController radController;
 
   const LocationRegisterWidget({
     super.key,
+    required this.nameController,
     required this.latController,
     required this.longController,
     required this.radController,
@@ -21,32 +23,38 @@ class LocationRegisterWidget extends StatelessWidget {
           CrossAxisAlignment.stretch, // 가로로 늘려서 필드들이 동일한 너비를 갖도록 함
       children: <Widget>[
         TextField(
+          controller: nameController,
+          decoration: const InputDecoration(labelText: 'Name'),
+        ),
+        const SizedBox(height: 16), // 텍스트필드 사이 간격
+        TextField(
           controller: latController,
-          decoration: const InputDecoration(labelText: '위도'),
+          decoration: const InputDecoration(labelText: 'Latitude'),
           keyboardType: const TextInputType.numberWithOptions(decimal: true),
         ),
         const SizedBox(height: 16), // 텍스트필드 사이 간격
         TextField(
           controller: longController,
-          decoration: const InputDecoration(labelText: '경도'),
+          decoration: const InputDecoration(labelText: 'Longitude'),
           keyboardType: const TextInputType.numberWithOptions(decimal: true),
         ),
         const SizedBox(height: 16), // 텍스트필드 사이 간격
         TextField(
           controller: radController,
-          decoration: const InputDecoration(labelText: '반경 (미터)'),
+          decoration: const InputDecoration(labelText: 'Radius (meters)'),
           keyboardType: const TextInputType.numberWithOptions(decimal: true),
         ),
-        const SizedBox(height: 16),
+        const SizedBox(height: 16), // 텍스트필드 사이 간격
         ElevatedButton(
           onPressed: () async {
             await geofenceRegister.registerGeofence(
+              name: nameController.text,
               lat: latController.text,
               long: longController.text,
               rad: radController.text,
             );
           },
-          child: const Text('지오펜스 등록'),
+          child: const Text('register geofence'),
         ),
       ],
     );
