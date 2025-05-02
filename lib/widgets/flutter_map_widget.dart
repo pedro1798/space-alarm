040195088276b21f7a0c3325789b68f34d4af9b4
pedro1798 <peter584aa@knu.ac.kr>
2025-05-02 16:41:42 +0900

    location 리스트를 받아 마커로 flutter_map에 표시하는 기능 추가 예정

--- lib/widgets/flutter_map_widget.dart
+++ lib/widgets/flutter_map_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:get/get.dart';
+import 'package:plata/controllers/location_controller.dart';
 
 class FlutterMapWidget extends StatefulWidget {
   const FlutterMapWidget({super.key});
@@ -12,6 +14,7 @@ class FlutterMapWidget extends StatefulWidget {
 
 class _FlutterMapWidgetState extends State<FlutterMapWidget> {
   final MapController mapController = MapController();
+  final LocationController locationController = Get.find<LocationController>();
   // MapController는 FlutterMap의 상태를 관리하는 컨트롤러입니다.
   // MapController를 사용하면 지도 중심 좌표, 줌 레벨 등을 제어할 수 있습니다.
 
@@ -36,6 +39,16 @@ class _FlutterMapWidgetState extends State<FlutterMapWidget> {
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'com.example.app',
+        ),
+        MarkerLayer(
+          markers: [
+            Marker(
+              point: LatLng(35.889230, 128.610263),
+              width: 80,
+              height: 80,
+              child: Icon(Icons.location_pin, color: Colors.red, size: 40),
+            ),
+          ],
         ),
       ],
     );
