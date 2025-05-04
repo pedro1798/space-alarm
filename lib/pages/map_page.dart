ed9c55ed6df249bb37d4253861fad9f33c17b649
pedro1798 <peter584aa@knu.ac.kr>
2025-05-04 21:38:26 +0900

    change ui

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:plata/widgets/flutter_map_widget.dart';
+import 'package:plata/widgets/location_list_widget.dart';
 
 class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(title: Text('map example')),
+      appBar: AppBar(title: Text('Space Alarm')),
       body: Center(
-        child: Padding(
-          // padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 32.0),
-          padding: EdgeInsets.only(
-            top: size.width * 0,
-            left: 0,
-            bottom: size.width * 0.4,
-            right: 0,
-          ),
-          child: FlutterMapWidget(),
+        child: Column(
+          children: [
+            Expanded(flex: 3, child: FlutterMapWidget()),
+            // const SizedBox(height: 16),
+            const Divider(),
+            const SizedBox(height: 8),
+            const Text('저장된 위치 목록', style: TextStyle(fontSize: 16)),
+            const SizedBox(height: 8),
+            Expanded(flex: 2, child: LocationListWidget()),
+          ],
         ),
       ),
     );
