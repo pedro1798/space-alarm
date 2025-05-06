d52a29c264fe47e040e2eb974e2c6c960d5b0c9a
pedro1798 <peter584aa@knu.ac.kr>
2025-05-06 16:22:39 +0900

    add notification related  code

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,20 @@ import 'package:flutter_map/flutter_map.dart';
 import 'handlers/location_permission_hander.dart';
 import 'package:plata/controllers/location_controller.dart';
 import 'handlers/geofence_register.dart';
-
-class InitialBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.put(ThemeController()); // ThemeController 의존성 주입
-    Get.put(LocationController()); // LocationController 의존성 주입
-    Get.put(MapController());
-  }
-}
+import 'handlers/notification_hander.dart';
 
 void main() async {
   // Before accesing any methods ensure you initialize the plugin:
   WidgetsFlutterBinding.ensureInitialized();
+
+  Get.put(ThemeController()); // ThemeController 의존성 주입
+  Get.put(LocationController()); // LocationController 의존성 주입
+  Get.put(MapController());
+
   await requestLocationPermissions(); // 먼저 위치 권한 요청
   await initGeofenceSystem();
-  runApp(GetMaterialApp(initialBinding: InitialBinding(), home: MyApp()));
+  await initNotification(); // 알림 초기화
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
