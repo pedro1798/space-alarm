bf4190a018f15218422b5a03b853e2ce2b375dfa
pedro1798 <peter584aa@knu.ac.kr>
2025-05-06 14:36:02 +0900

    encapsulate getLocation ftn

--- lib/controllers/location_controller.dart
+++ lib/controllers/location_controller.dart
@@ -2,6 +2,7 @@
 import 'package:get/get.dart';
 import 'package:plata/models/stored_location.dart';
 import 'package:plata/services/location_db.dart';
+import 'package:native_geofence/native_geofence.dart' as geo;
 
 class LocationController extends GetxController {
   RxList<StoredLocation> locations = <StoredLocation>[].obs;
@@ -15,6 +16,12 @@ class LocationController extends GetxController {
     loadLocations();
   }
 
+  /// 외부에서 사용할 수 있는 안전한 fetch 메서드
+  Future<List<StoredLocation>> getLocations() async {
+    return locations.toList(); // 현재 메모리에 있는 데이터 제공
+  }
+
+  /// 내부적으로 DB에서 데이터를 로드
   void loadLocations() async {
     isLoading.value = true; // 로딩 시작
     final data = await LocationDatabase.getLocations();
@@ -22,8 +29,21 @@ class LocationController extends GetxController {
     isLoading.value = false; // 로딩 완료
   }
 
-  Future<void> deleteLocation(String id) async {
-    await LocationDatabase.deleteLocation(id);
+  Future<void> deleteLocation(geo.Geofence location) async {
+    try {
+      // native geofence 삭제
+      await geo.NativeGeofenceManager.instance.removeGeofence(location);
+    } catch (e) {
+      Get.snackbar(
+        '삭제 실패',
+        '지오펜스 삭제에 실패했습니다: $e',
+        snackPosition: SnackPosition.BOTTOM,
+        duration: Duration(seconds: 2),
+      );
+      return;
+    }
+    // DB에서 삭제
+    await LocationDatabase.deleteLocation(location.id);
     // 삭제 후 로드
     loadLocations();
     Get.snackbar(
